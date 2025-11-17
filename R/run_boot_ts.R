a31c358f9de13c9c53c55f40d63b4fd620ed6890
Timothy Marsh <Timothy.D.C.Marsh@gmail.com>
2025-11-17 17:23:37 -0500

    Added to time series bootstrap

--- R/run_boot_ts.R
+++ R/run_boot_ts.R
@@ -11,12 +11,18 @@
 #' x <- residuals(ts_object)
 #' run_boot_ts(x, samps)
 
-run_boot_ts <- function(x, samps) {
-
-  boot_samps <- x[samps,]
-
+run_boot_ts <- function(x, R) {
   
+  n <- length(x)
+  
+  samps <- data.frame(matrix(nrow = n, ncol = R))
+
+  for (i in seq(1:R)) {
+    samps[,i] <- sample(x, n, replace = TRUE)
+  }
+
+  samps
   # to be returned, the mean of the process and the the prediction
-  c(mean(boot_samps), predict(boot_samps, n.ahead = 5))
+  #c(mean(boot_samps), predict(boot_samps, n.ahead = 5))
 
 }
--- R/ts_simple_resid_boot.R
+++ R/ts_simple_resid_boot.R
@@ -15,7 +15,7 @@ ts_simple_resid_boot <- function(df, n) {
   # add something here to check the type of data given and do different things with it
   x <- df
 
-  ts_model <- arima(data = x, order = c(n,0,0))
+  ts_model <- arima(x, order = c(n,0,0))
 
   # interesting data but unnecessary for our purposes here I think?
   #  Would it be useful to have some kind of output like this for the hospital project?
@@ -31,11 +31,24 @@ ts_simple_resid_boot <- function(df, n) {
 
   #boot function to run on that model
   #tsboot(resids, statistic = run_boot_ts, R = 500, sim = "model")
-  boot_resids <- boot::boot(data = resids, statistic = run_boot_ts, R = 500)
+  #boot_resids <- boot::boot(data = resids, statistic = run_boot_ts, R = 500)
+  R <- 500
+  boot_resids <- run_boot_ts(resids, R)
   
   #final bootstrapped iteration is something like this
   
   # this should be a data frame? which each column as a bootstrap replicate of values in the AR series
-  ts_booted <- ts_mean + coefs %*% x + boot_resids$t
+  #ts_booted <- ts_mean + coefs %*% x + boot_resids$t
+  
+  # initializing the data frame
+  boot_reps <- data.frame(matrix(nrow = length(x), ncol = R))
+  # a series of loops to build the R bootstrap replicates of the time series itself
+  for (i in seq(1:R)) {
+    for (j in seq(1:length(x))) {
+      boot_reps[i,j] <- ts_mean + coefs[1] * boot_reps[i,j-1] + boot_resids[i,j]
+    }
+  }
+  
+  ts_booted
 
 }
