9d84617ee674540fe5a6eea3dfb2286433420727
Timothy Marsh <Timothy.D.C.Marsh@gmail.com>
2026-06-11 17:07:52 -0400

    Updated typos and code to reflect theory

--- R/arx_mediation.R
+++ R/arx_mediation.R
@@ -29,65 +29,54 @@ arx_mediation <- function(X, M, Y, params = list(xp = 1, mp = 1, yp = 1)){
   
   # to estimate the covariances we will need the sigmas:
   sigmaX <- var(X) * (1-alpha[1]^2)
+  sigmaM <- var(M)
   
   # comparing theoretical covariances to observed covariances
   # remove NA's
   CovXM <- cov(X[-1],M[-1])
   # find covariance according to our theory
-  #CovXM_theory <- var(X) * ((beta[3]*alpha[1])/((1-alpha[1]^2)*(1-(beta[3]*alpha[1]) )))
-  #CovXM_theory <- var(X) * ((beta[3]^2)/((1-alpha[1]^2)*(1-(beta[3]^2) )))
-  #CovXM_theory <- var(X) * ((beta[3]^3)/((1-alpha[1]^2)^2*(1-(beta[1]^2) )*(1-alpha[1]^2)*(1-beta[1]^2)))
-  CovXM_theory <- ((beta[3]^2)/((1-alpha[1]^2)^2 * (1-beta[1]^2)) ) * sigmaX
-  diffXM <- abs(CovXM - CovXM_theory)
-  diffXMpercent <- (diffXM/CovXM)*100
+  CovXM_plug <- ((beta[3]^2)/((1-alpha[1]^2)^2 * (1-beta[1]^2)) ) * sigmaX
   
   CovXY <- cov(X[-c(1,2)], Y[-c(1,2)])
- # CovXY_theory <- var(X) * ((eta[4]^2)/((1-alpha[1]^2)*(1-eta[4]^2)))
-  CovXY_theory <- (1/(1-alpha[1]^2)) * ((beta[3]^2*eta[3]^2 + eta[4]^2 *(1-beta[1]^2))/((1-alpha[1]^2)*(1-beta[1]^2)*(1-eta[1]^2))) * sigmaX
-  diffXY <- abs(CovXY- CovXY_theory)
-  diffXYpercent <- (diffXY/CovXY)*100
+  CovXY_plug <- ((beta[3]^2*eta[3]^2 + eta[4]^2 *(1-beta[1]^2))/((1-alpha[1]^2)^2*(1-beta[1]^2)*(1-eta[1]^2))) * sigmaX
   
   CovMY <- cov(M[-c(1,2)], Y[-c(1,2)])
-  #CovMY_theory <- var(X) * ((eta[4]^2 * beta[3]^2)/((1-beta[3]^2)*(1-eta[4]^2))) + var(M[-1]) * ((eta[3]^2)/((1-beta[3]^2)*(1-eta[3]^2)))
-  CovMY_theory <- 1
-  diffMY <- abs(CovMY - CovMY_theory)
-  diffMYpercent <- (diffMY/CovMY)*100
+  CovMY_theory <- sigmaX * (eta[3]^2)/((1-beta[1]^2)^2 * (1-eta[1]^2)) + ((eta[3]^2 * beta[3]^4)+(eta[4]^2 * (1-beta[1]^2) * beta[3]^2))/((1-alpha^2)^2 * (1-beta[1]^2)^2 * (1-eta[1]^2))
   
-  list(XM = c(CovXM, CovXM_theory[[1]], diffXM[[1]],diffXMpercent), XY = c(CovXY, CovXY_theory[[1]], diffXY[[1]],diffXYpercent), MY = c(CovMY, CovMY_theory[[1]],diffMY[[1]],diffMYpercent))
+  list(XM = c(CovXM, CovXM_plug[[1]], XM_theory), XY = c(CovXY, CovXY_theory[[1]], XY_theory), MY = c(CovMY, CovMY_theory[[1]],MY_theory))
 }
 
-# testing :P
-X <- arima.sim(list( ar = 0.5), 102)
-m <- arima.sim(list(ar = 0.6), 101) + 0.2 * X[-102]
-
-y <- arima.sim(list(ar = 0.4), 100) + 0.3 * m[-101] + 0.5 * X[c(-101,-102)]
-M <- append(NA,m)
-Y <- append(c(NA,NA),y)
-
-xp <- 1
-mp <- 1
-yp <- 1
+# # testing :P
+# X <- arima.sim(list( ar = 0.5), 102)
+# m <- arima.sim(list(ar = 0.6), 101) + 0.2 * X[-102]
+# 
+# y <- arima.sim(list(ar = 0.4), 100) + 0.3 * m[-101] + 0.5 * X[c(-101,-102)]
+# M <- append(NA,m)
+# Y <- append(c(NA,NA),y)
+# 
+# xp <- 1
+# mp <- 1
+# yp <- 1
 
 # A function that simulates mutliple iterations of the previous function
-arx_mediation_summary <- function(iterations = 1000){
-  resultsXM <- data.frame(observed = NA, theoretical = NA, difference = NA, percentDiff =  NA)
-  resultsXY <- data.frame(observed = NA, theoretical = NA, difference = NA, percentDiff =  NA)
-  resultsMY <- data.frame(observed = NA, theoretical = NA, difference = NA, percentDiff =  NA)
+arx_mediation_summary <- function(iterations = 1000, length = 1000){
+  resultsXM <- data.frame(observed = NA, plug_in = NA, theoretical = NA)
+  resultsXY <- data.frame(observed = NA, plug_in = NA, theoretical = NA)
+  resultsMY <- data.frame(observed = NA, plug_in = NA, theoretical = NA)
+  
+  n <- length
+  alpha <- 0.5
+  beta <- c(0.2,0.3)
+  eta <- c(0.4,0.5,0.6)
+  
+  XM_theory <- (beta[2]^2)/((1-alpha^2)^2 * (1-beta[1]^2))
+  XY_theory <- ((eta[2]^2 * beta[2]^2)+(eta[3]^2 * (1-beta[1]^2)))/((1-alpha^2)^2 * (1-beta[1]^2) * (1-eta[1]^2))
+  MY_theory <- (eta[2]^2)/((1-beta[1]^2)^2 * (1-eta[1]^2)) + ((eta[2]^2 * beta[2]^4)+(eta[3]^2 * (1-beta[1]^2) * beta[2]^2))/((1-alpha^2)^2 * (1-beta[1]^2)^2 * (1-eta[1]^2))
   
   for (i in 1:iterations) {
-    #n <- runif(1, min = 1000, max = 1000)
-    # n <- 100
-    # X <- arima.sim(list(ar = runif(1,0.001,1)), n)
-    # M <- arima.sim(list(ar = runif(1,0.001,1)), (n-1)) + runif(1,0.001,1) * X[-n]
-    # Y <- arima.sim(list(ar = runif(1,0.001,1)), (n-2)) + runif(1,0.001,1) * M[-(n-1)] + runif(1,0.001,1) * X[-c(n-1,n)]
-    
-    n <- 10
-    alpha <- 0.5
-    beta <- c(0.2,0.3)
-    eta <- c(0.4,0.5,0.6)
-    X <- arima.sim(list(ar = alpha), n)
-    M <- arima.sim(list(ar = beta[1]), n-1) + beta[2] * X[-n]
-    Y <- arima.sim(list(ar = eta[1]), (n-2)) + eta[2] * M[-(n-1)] + eta[3] * X[-c(n-1,n)]
+    X <- arima.sim(list(ar = alpha), n, sd = 1)
+    M <- arima.sim(list(ar = beta[1]), n-1, sd = 1) + beta[2] * X[-n]
+    Y <- arima.sim(list(ar = eta[1]), (n-2), sd = 1) + eta[2] * M[-(n-1)] + eta[3] * X[-c(n-1,n)]
     
     M <- append(NA, M)
     Y <- append(c(NA,NA), Y)
@@ -102,6 +91,11 @@ arx_mediation_summary <- function(iterations = 1000){
   }
   
   list(XM = resultsXM, XY = resultsXY, MY = resultsMY, example_data)
-
 }
 
+# Usage
+# first focusing on cov(X,M)
+results <- arx_mediation_summary(iterations = 1000, length = 1000)
+XM <- results[[1]]
+percent_diff <- (XM$observed - XM$theoretical)/XM$theoretical
+hist(percent_diff)
