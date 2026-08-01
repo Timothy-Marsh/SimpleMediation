73768bdedc63bc52dccb8023cd151f865d556606
Timothy Marsh <Timothy.D.C.Marsh@gmail.com>
2026-07-31 20:13:57 -0400

    Prediction based function written up to issues with arima

--- R/prediction_arx.R
+++ R/prediction_arx.R
@@ -17,46 +17,65 @@
 #' prediction_arx(dat)
 #' 
 prediction_arx <- function(data, p = 10){
+  # construct models
+  mod_x <- arima(data[,1], order = c(1,0,0))
   mod_M <- arima(data[,2], order = c(1,0,0), xreg = data[,1])
   mod_Y <- arima(data[,3], order = c(1,0,0), xreg = data.frame(data[,1], data[,2]))
   
-  direct_effect <- mod_Y$coef
+  # construct test points
+  segment_length <- (max(data[,1])-min(data[,1]))/(p-1)
+  x_range <- seq(min(data[,1]), max(data[,1]), segment_length)
   
-  x_range <- seq(min(data[,1]), max(data[,1]), (max(data[,1])-min(data[,1]))/p)
-  m_range <- seq(min(data[,2]), max(data[,2]), (max(data[,2])-min(data[,2]))/p)
-  
-  total_change <- data.frame(m_range,x_range)
-  indirect_change <- data.frame(m_range, rep(mean(data[,1]),(p+1)))
-  direct_change <- data.frame(rep(mean(data[,2]),(p+1)), x_range)
-  baseline <- data.frame(rep(mean(data[,2]),(p+1)), rep(mean(data[,1]),(p+1)))
-  
-  TC <- c()
-  IC <- c()
-  DC <- c()
-  NC <- c()
-  
+  m_range <- c()
   for (i in 1:length(x_range)) {
-    TC[i] <- predict(mod_Y, newxreg = total_change[i,])$pred[1]
-    
-    IC[i] <- predict(mod_Y, newxreg = indirect_change[i,])$pred[1]
-    
-    DC[i] <- predict(mod_Y, newxreg = direct_change[i,])$pred[1]
-    
-    NC[i] <- predict(mod_Y, newxreg = baseline[i,])$pred[1]
+    m_range[i] <- predict(mod_M, newxreg = x_range[i])$pred[1]
   }
   
-  changes <- data.frame(all_change = TC, m_change = IC, x_change = DC, baseline = NC)
+  y_range <- c()
+  for (i in 1:length(x_range)) {
+    y_range[i] <- predict(mod_Y, newxreg = data.frame(x_range[i], m_range[i]))$pred[1]
+  }
   
-  direct <- changes$x_change - changes$baseline
-  #direct2 <- changes$m_change - changes$all_change
-  indirect <- changes$m_change - changes$baseline
-  #indirect2 <- changes$x_change - changes$all_change
-  #data.frame(direct = direct, direct2 = direct2, indirect = indirect, indirect2 = indirect2)
+  # construct the data frames to use for each estimate
+  N <- length(data[,1])
+  baseline_x <- data[(N-1),1]
+  baseline_m <- data[(N-1),2]
+  baseline_y <- data[(N-1),3]
   
-  direct_summary <- diff(direct)/((max(data[,1])-min(data[,1]))/p)
-  indirect_summary <- diff(indirect) / ((max(data[,2])-min(data[,2]))/p)
+  change_x <- data.frame(rep(baseline_m,p), x_range)
+  change_m <- data.frame(m_range, rep(baseline_x,p))
+  baseline <- data.frame(baseline_m, baseline_x)
   
-  c(indirect_summary[1], direct_summary[1])
+  baseline_prediction <- predict(mod_Y, newdata = baseline_y, newxreg = baseline)$pred[1]
+  
+  # create predictions for each quantity
+  # Natural Indirect Effect (NIE)
+  NIE <- c()
+  
+  # self-mediated direct effect
+  SMDE <- c()
+  
+  # cross-mediated direct effect
+  CMDE <- c()
+  
+  for (i in 1:length(x_range)) {
+    NIE[i] <- predict(mod_Y, newdata = baseline_y, newxreg = change_m[i,])$pred[1] - baseline_prediction
+    
+    SMDE[i] <- predict(mod_Y, newdata = baseline_y, newxreg = change_x[i,])$pred[1] - baseline_prediction
+    
+    CMDE[i] <- predict(mod_Y, newdata = y_range[i], newxreg = baseline)$pred[1] - baseline_prediction
+  }
+  
+  # find standardized differences
+  indirect_effect <- (NIE[2:length(x_range)] - NIE[1:(length(x_range)-1)])/segment_length
+  self_mediated_effect <- (SMDE[2:length(x_range)] - SMDE[1:(length(x_range)-1)])/segment_length
+  cross_mediated_effect <- (CMDE[2:length(x_range)] - CMDE[1:(length(x_range)-1)])/segment_length
+  
+  # output a list of vectors
+  list(indirect_effect = indirect_effect, self_mediated_effect = self_mediated_effect, cross_mediated_effect = cross_mediated_effect)
+  
+  # can find mechanistic estimates to compare to?
+  #direct_effect <- mod_Y$coef
 }
 
 # pred_arx_sum(boots)
@@ -68,4 +87,5 @@ pred_arx_sum <- function(boots){
   }
   
   out
+  
 }
