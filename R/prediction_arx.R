86ac771a94709cf25da3059d2a3b5c75e81c50a1
Timothy Marsh <Timothy.D.C.Marsh@gmail.com>
2026-07-31 23:06:02 -0400

    Changed the way the arx models are implemented to reflect the algorithm described in the theory. Also added a check for the theoretical way of computing the effects.

--- R/prediction_arx.R
+++ R/prediction_arx.R
@@ -18,35 +18,43 @@
 #' 
 prediction_arx <- function(data, p = 10){
   # construct models
-  mod_x <- arima(data[,1], order = c(1,0,0))
-  mod_M <- arima(data[,2], order = c(1,0,0), xreg = data[,1])
-  mod_Y <- arima(data[,3], order = c(1,0,0), xreg = data.frame(data[,1], data[,2]))
+  N <- length(data[,1])
+  data = data.frame(x = data[,1][-1], m = data[,2][-1], y = data[,3][-1], xlag = data[,1][-N], mlag = data[,2][-N], ylag = data[,3][-N])
+  mod_x <- lm(x ~ xlag, data = data)
+  mod_M <- lm(m ~ mlag + xlag, data = data)
+  mod_Y <- lm(y ~ ylag + mlag + xlag, data = data)
   
   # construct test points
   segment_length <- (max(data[,1])-min(data[,1]))/(p-1)
-  x_range <- seq(min(data[,1]), max(data[,1]), segment_length)
+  x_range_initial <- seq(min(data[,1]), max(data[,1]), segment_length)
+  
+  x_range <- c()
+  j <- length(x_range_initial)
+  for (i in 1:j) {
+    x_range[i] <- predict(mod_x, newdata = data.frame(xlag = x_range_initial[i]))
+  }
   
   m_range <- c()
   for (i in 1:length(x_range)) {
-    m_range[i] <- predict(mod_M, newxreg = x_range[i])$pred[1]
+    m_range[i] <- predict(mod_M, newdata = data.frame(xlag = x_range_initial[i], mlag = data$m[(N-1)]))
   }
   
   y_range <- c()
   for (i in 1:length(x_range)) {
-    y_range[i] <- predict(mod_Y, newxreg = data.frame(x_range[i], m_range[i]))$pred[1]
+    y_range[i] <- predict(mod_Y, newdata = data.frame(xlag = x_range_initial[i], mlag = m_range[i], ylag = data$y[(N-1)]))
   }
   
   # construct the data frames to use for each estimate
-  N <- length(data[,1])
-  baseline_x <- data[(N-1),1]
-  baseline_m <- data[(N-1),2]
-  baseline_y <- data[(N-1),3]
+  baseline_x <- tail(data$x,1)
+  baseline_m <- tail(data$m,1)
+  baseline_y <- tail(data$y,1)
   
-  change_x <- data.frame(rep(baseline_m,p), x_range)
-  change_m <- data.frame(m_range, rep(baseline_x,p))
-  baseline <- data.frame(baseline_m, baseline_x)
+  change_x <- data.frame(mlag = rep(baseline_m,p), xlag = x_range, ylag = rep(baseline_y,p))
+  change_m <- data.frame(mlag = m_range, xlag = rep(baseline_x,p), ylag = rep(baseline_y,p))
+  change_y <- data.frame(mlag = rep(baseline_m,p), xlag = rep(baseline_x,p), ylag = y_range)
+  baseline <- data.frame(mlag = baseline_m, xlag = baseline_x, ylag = baseline_y)
   
-  baseline_prediction <- predict(mod_Y, newdata = baseline_y, newxreg = baseline)$pred[1]
+  baseline_prediction <- predict(mod_Y, newdata = baseline)
   
   # create predictions for each quantity
   # Natural Indirect Effect (NIE)
@@ -59,11 +67,11 @@ prediction_arx <- function(data, p = 10){
   CMDE <- c()
   
   for (i in 1:length(x_range)) {
-    NIE[i] <- predict(mod_Y, newdata = baseline_y, newxreg = change_m[i,])$pred[1] - baseline_prediction
+    NIE[i] <- predict(mod_Y, newdata = change_m[i,]) - baseline_prediction
     
-    SMDE[i] <- predict(mod_Y, newdata = baseline_y, newxreg = change_x[i,])$pred[1] - baseline_prediction
+    SMDE[i] <- predict(mod_Y, newdata = change_x[i,]) - baseline_prediction
     
-    CMDE[i] <- predict(mod_Y, newdata = y_range[i], newxreg = baseline)$pred[1] - baseline_prediction
+    CMDE[i] <- predict(mod_Y, newdata = change_y[i,]) - baseline_prediction
   }
   
   # find standardized differences
@@ -71,11 +79,13 @@ prediction_arx <- function(data, p = 10){
   self_mediated_effect <- (SMDE[2:length(x_range)] - SMDE[1:(length(x_range)-1)])/segment_length
   cross_mediated_effect <- (CMDE[2:length(x_range)] - CMDE[1:(length(x_range)-1)])/segment_length
   
-  # output a list of vectors
-  list(indirect_effect = indirect_effect, self_mediated_effect = self_mediated_effect, cross_mediated_effect = cross_mediated_effect)
+  # can find mechanistic estimates to compare to? THIS IS ONLY FOR ARX BASED APPROACH
+  theory_NIE <- mod_Y$coefficients["mlag"] * mod_M$coefficients["xlag"]
+  theory_SMDE <- mod_Y$coefficients["xlag"] * mod_x$coefficients["xlag"]
+  theory_CMDE <- mod_Y$coefficients["ylag"] * mod_Y$coefficients["xlag"]
   
-  # can find mechanistic estimates to compare to?
-  #direct_effect <- mod_Y$coef
+  # output a list of vectors
+  list(indirect_effect = indirect_effect, self_mediated_effect = self_mediated_effect, cross_mediated_effect = cross_mediated_effect, theory_NIE = theory_NIE, theory_SMDE = theory_SMDE, theory_CMDE = theory_CMDE)
 }
 
 # pred_arx_sum(boots)
